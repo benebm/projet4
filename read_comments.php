7e3494e68bd5bcb7d54312722cfaa977f9659032
benebm <31929996+benebm@users.noreply.github.com>
2017-12-28 14:41:43 +0100

    Chargement de 4 nouveaux fichiers

---
+++ read_comments.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Mon blog</title>
+	<link href="style.css" rel="stylesheet" /> 
+    </head>
+        
+    <body>
+        <h1>Mon super blog !</h1>
+        <p class="news"><a href="index.php">Retour à la liste des billets</a></p>
+ 
+<?php
+// Connexion à la base de données
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+
+// Récupération du billet
+$req = $bdd->prepare('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets WHERE id = ?');
+$req->execute(array($_GET['billet']));
+$donnees = $req->fetch();
+?>
+
+<div class="news">
+    <h3>
+        <?php echo htmlspecialchars($donnees['titre']); ?>
+        <em>le <?php echo $donnees['date_creation_fr']; ?></em>
+    </h3>
+    
+    <p>
+    <?php
+    echo nl2br(htmlspecialchars($donnees['contenu']));
+    ?>
+    </p>
+</div>
+
+<div class = "comments">
+<h2>Commentaires</h2>
+
+<h4>Poster un commentaire</h4>
+
+<form action="save_comments.php?billet=<?php echo $donnees['id']; ?>" method="post">
+        <p>
+        <label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" id="pseudo" /><br />
+        <label for="message">Message</label> :  <br /><textarea name="message" id="message" rows="5" cols="50">Votre commentaire</textarea><br />
+        
+        <input type="submit" value="Envoyer" />
+    </p>
+    </form>
+
+<h4>Commentaires récents</h4>
+
+<?php
+$req->closeCursor(); // Important : on libère le curseur pour la prochaine requête
+
+// Récupération des commentaires
+$req = $bdd->prepare('SELECT auteur, commentaire, DATE_FORMAT(date_commentaire, \'%d/%m/%Y à %Hh%imin%ss\') AS date_commentaire_fr FROM commentaires WHERE id_billet = ? ORDER BY date_commentaire DESC');
+$req->execute(array($_GET['billet']));
+
+while ($donnees = $req->fetch())
+{
+?>
+<p><strong><?php echo htmlspecialchars($donnees['auteur']); ?></strong> le <?php echo $donnees['date_commentaire_fr']; ?></p>
+<p><?php echo nl2br(htmlspecialchars($donnees['commentaire'])); ?></p>
+<?php
+} // Fin de la boucle des commentaires
+$req->closeCursor();
+?>
+</div>
+</body>
+</html>
---
+++ save_comments.php
@@ -0,0 +1,22 @@
+<?php
+// Connexion à la base de données
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+
+
+
+// Insertion du message à l'aide d'une requête préparée
+$req = $bdd->prepare('INSERT INTO commentaires (id_billet, auteur, commentaire, date_commentaire) VALUES(?, ?, ?, NOW())');
+$req->execute(array($_GET['billet'], $_POST['pseudo'], $_POST['message']));
+$req->closeCursor();
+
+
+// Redirection du visiteur vers la page du minichat
+header('Location: read_comments.php?billet='.$_GET['billet'].'');
+?>
